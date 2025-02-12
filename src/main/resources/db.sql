eabf652710780919e0072bcce8f9d747890068c2
youcefmei <youcefmei@aol.com>
2025-02-12 09:52:04 +0100

    cicd: use in db

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -18,15 +18,15 @@
 --
 -- Table structure for table `person`
 --
-
+USE springboot;
 DROP TABLE IF EXISTS `person`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `person` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `lastname` varchar(255) DEFAULT NULL,
-  `firstname` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+                          `id` int unsigned NOT NULL AUTO_INCREMENT,
+                          `lastname` varchar(255) DEFAULT NULL,
+                          `firstname` varchar(255) DEFAULT NULL,
+                          PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -49,4 +49,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-02-06 16:11:13
+-- Dump completed on 2025-02-12  9:40:49
